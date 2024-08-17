940a27c4c4e5895f3256bf39294d515698ce0913
Ehsan <1382ehg@gmail.com>
2024-08-17 20:36:01 +0330

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ava_project/"
+  base: "/ava_project/",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://harf.roshan-ai.ir',  // Replace with your actual API base URL
+        changeOrigin: true,  // Needed for virtual hosted sites
+        secure: true,       // Set to true if you're using https
+        // rewrite: (path) => path.replace(/^\/api/, ''),  // Remove /api prefix if needed
+      },
+    },
+  },
 })
