978549f2db00753cffaffe49c3f092c9b240a7f7
Ehsan <1382ehg@gmail.com>
2024-08-17 21:05:37 +0330

    test set secure  to false

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://harf.roshan-ai.ir',  // Replace with your actual API base URL
         changeOrigin: true,  // Needed for virtual hosted sites
-        secure: true,       // Set to true if you're using https
+        secure: false,       // Set to true if you're using https
         // rewrite: (path) => path.replace(/^\/api/, ''),  // Remove /api prefix if needed
       },
     },
